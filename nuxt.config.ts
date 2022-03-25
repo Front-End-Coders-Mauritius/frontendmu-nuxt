3b0ce1854326298eddc347f93c1e20968ca20cf9
mramgolam <mahima.ramgolam@prodigious.com>
2022-03-25 18:58:57 +0400

    test OG image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,4 +15,19 @@ export default defineNuxtConfig({
     css: [
         "~/assets/css/tailwind.css"
     ],
+    head: {
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'my website description'
+          },
+          { property: "og:image:width", content: "740" },
+          { property: "og:image:height", content: "300" },
+          { name: "twitter:site", content: "/img/meetupFEC.png" },
+          { name: "twitter:card", content: "/img/meetupFEC.png" },
+        ],
+      }
 })
