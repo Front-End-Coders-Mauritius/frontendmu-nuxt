f05343932a844f9465570f1b8f06466a44aa6daf
Mahima Ramgolam <mahima7691487@gmail.com>
2022-03-26 01:09:22 +0400

    updates OG image

--- components/MetaTag.vue
+++ components/MetaTag.vue
@@ -1,27 +1,30 @@
 <template>
-    <Html>
-        <Head>
-            <Title>{{ eventName }}</Title>
-            <Meta name="description" :content="`${eventDetail}`" />
-            <Meta property="og:title" :content="`${eventName}`" />
-            <Meta property="og:site_name" :content="`${eventName}`" />
-            <Meta property="og:url" content="https://front-end-coders-mauritius.netlify.app/" />
-            <Meta property="og:description" content />
-            <Meta property="og:type" content="website" />
-            <Meta property="og:image" content="/img/meetupFEC.png" />
-            <Link rel="icon" type="image/png" href="/img/favicon.png" />
-        </Head>
-    </Html>
+  <Html>
+    <Head>
+      <Title>{{ eventName }}</Title>
+      <Meta name="description" :content="`${eventDetail}`" />
+      <Meta property="og:title" :content="`${eventName}`" />
+      <Meta property="og:site_name" :content="`${eventName}`" />
+      <Meta
+        property="og:url"
+        content="https://front-end-coders-mauritius.netlify.app/"
+      />
+      <Meta property="og:description" content />
+      <Meta property="og:type" content="website" />
+      <Meta property="og:image" content="/img/ogimage.png" />
+      <Link rel="icon" type="image/png" href="/img/favicon.png" />
+    </Head>
+  </Html>
 </template>
 <script>
 export default {
-    props: ['eventName', 'eventDetails'],
+  props: ["eventName", "eventDetails"],
 
-    computed: {
-        eventDetail() {
-            const eventDescript = this.eventDetails.replace(/<\/?[^>]+>/gi, "");
-            return eventDescript;
-        }
-    }
-}
+  computed: {
+    eventDetail() {
+      const eventDescript = this.eventDetails.replace(/<\/?[^>]+>/gi, "");
+      return eventDescript;
+    },
+  },
+};
 </script>
