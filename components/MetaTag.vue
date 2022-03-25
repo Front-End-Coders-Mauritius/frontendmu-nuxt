f99c625a792f8112b5f13af964ed04d98bb6ca85
mramgolam <mahima.ramgolam@prodigious.com>
2022-03-25 18:45:51 +0400

    test og image

--- components/MetaTag.vue
+++ components/MetaTag.vue
@@ -3,6 +3,10 @@
         <Head>
             <Title>{{ eventName }}</Title>
             <Meta name="description" :content="`${eventDetails}`" />
+            <Meta property="og:title" content="Front-end Coder Mauritius" />
+            <Meta property="og:type" content="Meetups" />
+            <Meta property="og:url" content="https://front-end-coders-mauritius.netlify.app/" />
+            <Meta property="og:image" content="/img/meetupFEC.png" />
             <Link rel="icon" type="image/png" href="/img/favicon.png" />
         </Head>
     </Html>
