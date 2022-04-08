b61e0238bfae65862625930e88d61fba086b776f
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-04-08 15:46:53 +0400

    updated last session for meetup

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,7 +1,6 @@
 <template>
-    <div>
+    <div class="md:hover:first-letter:color-red-500">
         <Header />
         <router-view :key="$route.params?.id" />
     </div>
-</template>  
-
+</template>
