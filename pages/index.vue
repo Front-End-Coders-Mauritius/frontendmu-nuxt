d9f0701e3b0e20fb3e0ef522782fe7fd7e98bcdd
Mahima Ramgolam <mahima7691487@gmail.com>
2022-04-10 16:09:51 +0400

    updated alignment for latest event icon

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,11 @@
     <div class="homepage-wrapper">
       <main class>
         <div class="w-full h-[700px] md:h-[800px] relative z-0">
-          <v-vanta class="absolute w-full z-0" effect="rings" :options="options"></v-vanta>
+          <v-vanta
+            class="absolute w-full z-0"
+            effect="rings"
+            :options="options"
+          ></v-vanta>
           <div
             class="fade-out absolute z-10 h-screen w-full bg-gradient-to-t from-white to-transparent"
           ></div>
@@ -16,7 +20,9 @@
               <h1
                 class="text-4xl flex flex-col tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl"
               >
-                <span class="block xl:inline text-black">Front-End Coders Mauritius</span>
+                <span class="block xl:inline text-black"
+                  >Front-End Coders Mauritius</span
+                >
               </h1>
               <p class="text-lg text-gray-700 sm:text-xl">
                 Front-End coders Mauritius is a free meetup that's dedicated to
@@ -27,7 +33,8 @@
                 <router-link
                   to="/events"
                   class="rounded-md shadow w-64 mx-auto flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium text-white bg-yellow-500 hover:bg-yellow-400 md:py-4 md:text-lg md:px-10"
-                >View Events</router-link>
+                  >View Events</router-link
+                >
               </div>
             </div>
           </div>
@@ -36,7 +43,9 @@
     </div>
   </div>
   <div class="latest-events-container sm:py-6 md:pt-8 relative z-20">
-    <div class="latest-events-wrapper md:max-w-2xl lg:max-w-7xl mx-auto pt-8 px-4 md:px-0">
+    <div
+      class="latest-events-wrapper md:max-w-2xl lg:max-w-7xl mx-auto pt-8 px-4 md:px-0"
+    >
       <div
         class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-5xl py-4 md:py-8"
       >
@@ -61,7 +70,9 @@
           ]"
         >
           <div>
-            <span class="rounded-lg inline-flex p-3 ring-4 ring-white bg-blue-50 text-blue-700">
+            <span
+              class="rounded-lg inline-flex p-3 ring-4 ring-white bg-blue-50 text-blue-700"
+            >
               <ClockIcon class="h-6 w-6 mr-2" />
               <span>{{ new Date(event.local_date).toDateString() }}</span>
             </span>
@@ -81,12 +92,16 @@
                       ? 'bg-green-100 text-green-800 tagStyle'
                       : 'hidden',
                   ]"
-                >{{ event.status }}</p>
+                >
+                  {{ event.status }}
+                </p>
               </router-link>
             </h3>
 
             <div class="flex flex-col border-gray-100 pt-4 md:pt-6 lg:pt-2">
-              <div class="flex text-base justify-start items-center font-medium text-gray-400">
+              <div
+                class="flex text-base justify-start items-center font-medium text-gray-400"
+              >
                 <LocationMarkerIcon
                   class="flex-shrink-0 mr-1.5 h-4 w-4 text-gray-400 truncate"
                   aria-hidden="true"
@@ -94,13 +109,15 @@
                 <div class="pt-1">Venue: {{ event.venue.name }}</div>
               </div>
               <div
-                class="flex text-base justify-start items-center font-medium text-gray-400 leading-4"
+                class="flex text-base justify-start items-center font-medium text-gray-400 leading-5"
               >
                 <UsersIcon
-                  class="flex-shrink-0 mr-1.5 h-4 w-4 text-gray-400 truncate"
+                  class="flex-shrink-0 mr-1.5 h-[15px] w-[15px] text-gray-400 truncate"
                   aria-hidden="true"
                 />
-                <div class="pt-1">Attendees: {{ event.yes_rsvp_count }}</div>
+                <div class="pt-[2px]">
+                  Attendees: {{ event.yes_rsvp_count }}
+                </div>
               </div>
             </div>
           </div>
@@ -125,7 +142,8 @@
         <router-link
           to="/events"
           class="text-center bg-blue-600 text-white text-md md:text-xl px-4 md:px-8 py-4 rounded-md font-medium w-48 md:w-64"
-        >View all events</router-link>
+          >View all events</router-link
+        >
       </div>
     </div>
   </div>
@@ -164,12 +182,12 @@ export default {
         touchControls: true,
         gyroControls: false,
         minHeight: process.client ? window.innerHeight : 0,
-        minWidth: 200.00,
+        minWidth: 200.0,
         scale: 0.3,
-        scaleMobile: 1.00,
+        scaleMobile: 1.0,
         backgroundColor: 0xffffff,
         color: 0x59ff,
-        backgroundAlpha: 0.00
+        backgroundAlpha: 0.0,
       },
     };
   },
