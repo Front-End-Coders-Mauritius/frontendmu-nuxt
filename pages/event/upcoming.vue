de3df47090c075172a64a48abed104135a2b0aa1
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-04-22 20:55:20 +0400

    added directus sdk to talk to directus cloud for FEC

---
+++ pages/event/upcoming.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        {{ events || 'loading...' }}
+    </div>
+</template>
+
+<script setup>
+import { Directus } from "@directus/sdk";
+import { ref } from 'vue'
+
+const URL = 'https://l4yporup.directus.app'
+const directus = new Directus(URL);
+const events = ref(null)
+
+fetchData();
+
+async function fetchData() {
+    const response = await directus.items("Events").readByQuery();
+
+    // console.log(response);
+    events.value = response
+}
+</script>
