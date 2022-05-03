b9db87a1d01c04f8f46266de07abea6a5ac3aed3
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-05-03 15:06:01 +0400

    added seeder added events composable added filter by eventid

---
+++ types/types.ts
@@ -0,0 +1,23 @@
+export interface IEvent {
+    "id": string;
+    "title": string;
+    "description": string;
+    "Venue": string;
+    "Date": string;
+    "Time": string;
+    "Attendees": string;
+    "gallery": string[];
+}
+
+export interface IEvents {
+    data: IEvent[];
+}
+
+//  Mappings
+// "title"          =>  eventDetails?.name
+// "description"    =>  eventDetails?.description
+// "Venue"          =>  eventDetails?.venue
+// "Date"           =>  eventDetails?.local_date
+// "Time"           =>  eventDetails?.local_time
+// "Attendees"      =>  eventDetails?.rsvp_limit
+// "gallery"        =>  eventDetails?.images
