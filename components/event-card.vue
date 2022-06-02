262adc6d5a88deb233d7125f5e18d90aa9e0d97f
Mahima Ramgolam <mahima7691487@gmail.com>
2022-06-02 22:37:28 +0400

    flex-start the date on latest-event

--- components/event-card.vue
+++ components/event-card.vue
@@ -55,7 +55,7 @@ const isUpcoming = (currentEventDate: string) => {
 
 <template>
   <div
-    class="mt-4 relative rounded-xl flex flex-col md:flex-row items-center md:justify-start justify-center gap-8 group bg-white p-6 shadow-md"
+    class="mt-4 md:mt-0 relative rounded-xl flex flex-col md:flex-row items-start md:items-center gap-8 group bg-white p-6 shadow-md"
   >
     <div v-if="event.Date" class="">
       <span
