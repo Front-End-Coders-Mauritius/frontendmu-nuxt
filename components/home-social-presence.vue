bb353e5c15404bb700952740f0f4772f19d370b8
Mahima Ramgolam <mahima7691487@gmail.com>
2022-06-02 22:26:15 +0400

    updated home-social-presence on responsove

--- components/home-social-presence.vue
+++ components/home-social-presence.vue
@@ -1,54 +1,57 @@
 <template>
   <div class="bg-gray-900">
-    <div class="max-w-6xl mx-auto py-24 grid gap-8 grid-cols-2">
+    <div
+      class="max-w-6xl mx-auto py-24 grid gap-8 grid-rows-4 grid-cols-0 md:grid-cols-2 md:grid-rows-none px-8 md:px-4"
+    >
       <social-card
         href="https://discord.gg/WxXW9Jvv6k"
-        class="bg-[#5865f2] "
+        class="bg-[#5865f2] text-sm md:text-normal"
       >
         <template #tagline>Discord</template>
-        <template #description>Join the community, ask questions, livestream, chill! </template>
+        <template #description
+          >Join the community, ask questions, livestream, chill!
+        </template>
         <template #icon>
-          <icon-discord class="w-16" />
+          <icon-discord class="w-16 md:w-16" />
         </template>
       </social-card>
 
       <social-card
         href="https://github.com/Front-End-Coders-Mauritius"
-        class="bg-[#333]"
+        class="bg-[#333] text-sm md:text-normal"
       >
         <template #tagline>GitHub</template>
         <template #description>The home of FEC projects</template>
         <template #icon>
-          <icon-github class="w-16" />
+          <icon-github class="w-12 md:w-16" />
         </template>
       </social-card>
 
       <social-card
         href="https://www.facebook.com/groups/frontendcodersmu"
-        class="bg-[#4267B2] "
+        class="bg-[#4267B2] text-sm md:text-normal"
       >
         <template #tagline>Facebook Group</template>
         <template #description>Our legacy presence on Facebook.</template>
         <template #icon>
-          <icon-facebook-group class="w-16 p-2" />
+          <icon-facebook-group class="w-14 md:w-16 p-2" />
         </template>
       </social-card>
 
       <social-card
         href="https://www.linkedin.com/company/81846464/admin/"
-        class="bg-[#007db1] "
+        class="bg-[#007db1] text-sm md:text-normal"
       >
         <template #tagline>LinkedIn Page</template>
-        <template #description>For those who don't use facebook and discord </template>
+        <template #description
+          >For those who don't use facebook and discord
+        </template>
         <template #icon>
-          <icon-linkedin class="w-16" />
+          <icon-linkedin class="w-14 md:w-16" />
         </template>
       </social-card>
-
     </div>
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
