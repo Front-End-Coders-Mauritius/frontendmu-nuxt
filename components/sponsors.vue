e32d3a3085dd36551e1cb1657953d286bb935b4e
Mahima Ramgolam <mahima7691487@gmail.com>
2022-06-04 15:53:27 +0400

    Added sponsor section for ringier

---
+++ components/sponsors.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="flex flex-col justify-center items-center px-4">
+    <div class="flex flex-col justify-center items-center gap-8 py-16">
+      <p class="text-gray-400 font-bold text-2xl">Sponsors</p>
+      <logo-ringier class="w-64 h-auto opacity-80" />
+    </div>
+    <div
+      class="w-96 md:w-[700px] lg:w-[1050px] border-t-2 border-gray-100 p-4 text-center text-gray-500 font-light text-xs md:text-normal"
+    >
+      Copyright © 2022-present Front-End Coders Mauritius
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,29 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <script setup lang="ts">
-import HomeSocialPresence from '../components/home-social-presence.vue'
-import MetaTag from '~/components/MetaTag.vue'
-definePageMeta({
-  layout: 'home',
-  transition: {
-    name: 'index',
-  },
-})
+import HomeSocialPresence from "../components/home-social-presence.vue";
+import MetaTag from "~/components/MetaTag.vue";
+import sponsors from "~/components/sponsors.vue";
 
-const title = 'Front-End Coders Mauritius'
-const description
-  = 'Community of Front-End developers who share their passions for the web. Events, workshops and conferences occurs regularly.'
+definePageMeta({
+  layout: "home",
+  transition: {
+    name: "index",
+  },
+});
+
+const title = "Front-End Coders Mauritius";
+const description =
+  "Community of Front-End developers who share their passions for the web. Events, workshops and conferences occurs regularly.";
 </script>
 
 <template>
-  <meta-tag
-    :event-name="title"
-    :event-details="description"
-  />
+  <meta-tag :event-name="title" :event-details="description" />
 
   <home-hero />
 
   <home-social-presence />
 
   <home-latest-meetups />
+
+  <sponsors />
 </template>
