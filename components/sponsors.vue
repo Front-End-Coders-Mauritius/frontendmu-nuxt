4eb12eee830f31e2e79642d9a6814109a9b0fd7f
Mahima Ramgolam <mahima7691487@gmail.com>
2022-06-26 21:27:18 +0400

    Added Ringier Sponsor logo

--- components/sponsors.vue
+++ components/sponsors.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-import Vue from "vue";
-
-const props = defineProps({
-  backgroundColor: String,
-});
-</script>
-
 <template>
   <section
     :class="`${props?.backgroundColor} flex flex-col justify-center items-center px-4`"
@@ -14,10 +6,9 @@ const props = defineProps({
       class="flex flex-col justify-center items-center gap-6 md:gap-8 py-8 md:pt-16 md:pb-10"
     >
       <p class="text-gray-400 font-bold text-2xl">Sponsors</p>
-      <!-- <logo-ringier class="w-48 md:w-64 h-8 opacity-80" /> -->
-      <div class="text-3xl font-bold opacity-50 italic text-yellow-600">
-        YOUR LOGO HERE
-      </div>
+      <button @click="goToRingier">
+          <logo-ringier class="w-48 md:w-64 h-8 opacity-80" />
+      </button>
     </div>
     <div
       class="w-96 md:w-[700px] lg:w-[1050px] border-t-2 border-gray-100 p-4 text-center text-gray-500 font-light text-xs md:text-normal"
@@ -26,3 +17,16 @@ const props = defineProps({
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+import Vue from "vue";
+
+const props = defineProps({
+  backgroundColor: String,
+});
+
+function goToRingier() {
+  const navUrl = "https://www.ringier.com/about-us/south-africa/"
+  window.open(navUrl, "_blank");
+}
+</script>
