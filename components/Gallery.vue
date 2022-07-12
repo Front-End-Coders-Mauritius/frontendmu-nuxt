e0e0499ed17c0bb4cdebb6fe148de0555dff60cb
Mahima Ramgolam <mahima7691487@gmail.com>
2022-07-12 19:00:34 +0400

    gallery issue, revert back

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,24 +1,21 @@
 <template>
   <div>
     <client-only>
-      <Splide :options="{ rewind: true }" aria-label="My Favorite Images">
-          <SplideSlide v-for="item in galleryList" :key="item">
-            <img :src="item" alt="Sample 1">
-          </SplideSlide>
-      </Splide>
+      <vue-picture-swipe
+        :items="imagesList === null ? galleryList : imagesList"
+        :key="imagesList === null ? galleryList : imagesList"
+      ></vue-picture-swipe>
     </client-only>
   </div>
 </template>
 <script>
-import { Splide, SplideSlide } from '@splidejs/vue-splide';
-import '@splidejs/vue-splide/css';
+import VuePictureSwipe from "vue3-picture-swipe";
 
 export default {
   props: ["eventImages"],
 
   components: {
-    Splide,
-    SplideSlide,
+    VuePictureSwipe,
   },
 
   data: () => {
@@ -35,9 +32,12 @@ export default {
         return [];
       }
       this.eventImages.forEach((element) => {
-        this.myArray.push(
-          `https://l4yporup.directus.app/assets/${element.directus_files_id}`,
-        );
+        this.myArray.push({
+          src: `https://l4yporup.directus.app/assets/${element.directus_files_id}`,
+          thumbnail: `https://l4yporup.directus.app/assets/${element.directus_files_id}`,
+          w: 1280,
+          h: 720,
+        });
       });
 
       return this.myArray;
@@ -48,9 +48,12 @@ export default {
         return [];
       }
       this.eventImages.forEach((element) => {
-        this.myArray.push(
-           element.imagename,
-        );
+        this.myArray.push({
+          src: element.imagename,
+          thumbnail: element.imagename,
+          w: 1280,
+          h: 720,
+        });
       });
 
       return this.myArray;
@@ -62,13 +65,25 @@ export default {
     },
   },
 
+  mounted() {
+    console.log(this.eventImages);
+  },
+
   beforeUpdate() {
     this.clear;
   },
 };
 </script>
 <style>
-img {
-  @apply rounded-3xl lg:rounded-[3.5rem];
+.my-gallery {
+  @apply grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8;
+}
+
+.my-gallery figure {
+  @apply block w-full h-[200px] overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100;
+}
+
+.pswp img {
+  @apply object-contain;
 }
 </style>
