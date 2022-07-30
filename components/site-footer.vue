268eaec21ef0c03ce78e095f0b80d61e9f470f6e
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-07-30 19:35:31 +0400

    update sponsor section

---
+++ components/site-footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+
+    <div
+      class="w-full border-t-2 border-gray-100 p-4 text-center text-gray-500 font-light text-xs md:text-normal"
+    >
+      2022-present Front-End Coders Mauritius
+    </div>
+  </section>
+</template>
--- components/sponsors.vue
+++ components/sponsors.vue
@@ -1,32 +1,17 @@
 <template>
   <section
-    :class="`${props?.backgroundColor} flex flex-col justify-center items-center px-4`"
+    class="flex flex-col justify-center items-center px-4 bg-gray-100"
   >
     <div
-      class="flex flex-col justify-center items-center gap-6 md:gap-8 py-8 md:pt-16 md:pb-10"
+      class="flex py-8 justify-center items-center gap-6 md:gap-8 "
     >
-      <p class="text-gray-400 font-bold text-2xl">Sponsors</p>
-      <button @click="goToRingier">
-          <logo-ringier class="w-48 md:w-64 h-8 opacity-80" />
-      </button>
-    </div>
-    <div
-      class="w-96 md:w-[700px] lg:w-[1050px] border-t-2 border-gray-100 p-4 text-center text-gray-500 font-light text-xs md:text-normal"
-    >
-      2022-present Front-End Coders Mauritius
+      <p class="text-gray-300 font-bold text-4xl tracking-wide uppercase ">Sponsors</p>
+      <div class="flex flex-col justify-center items-center">
+        <a target="_blank" href="https://www.ringier.com/about-us/south-africa/">
+          <logo-ringier class="w-48 h-auto opacity-80" />
+        </a>
+        <div class="bg-blue-100 py-1 px-4 text-xs w-full text-center text-blue-500 rounded-2xl">Domain name sponsor</div>
+      </div>
     </div>
   </section>
 </template>
-
-<script setup lang="ts">
-import Vue from "vue";
-
-const props = defineProps({
-  backgroundColor: String,
-});
-
-function goToRingier() {
-  const navUrl = "https://www.ringier.com/about-us/south-africa/"
-  window.open(navUrl, "_blank");
-}
-</script>
