64730e9aa03050fdecd40e798f4178041f18328f
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-08-05 13:18:08 +0400

    updated hero section font and animation

--- components/home-hero.vue
+++ components/home-hero.vue
@@ -10,16 +10,16 @@
             class="relative z-20 mx-auto flex flex-col-reverse md:flex-row h-full justify-center max-w-6xl px-12 lg:px-0"
           >
             <div
-              class="flex flex-col justify-center text-center md:text-left gap-10 md:gap-16 md:w-1/2"
+              class="flex flex-col justify-center text-center md:text-left gap-10  md:w-1/2"
             >
               <h1
-                class="flex flex-col text-4xl font-extrabold tracking-tight font-mono text-blue-500 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl"
+                class="uppercase flex flex-col text-4xl font-extrabold xl:leading-tight text-blue-500 sm:text-5xl lg:text-5xl xl:text-6xl"
               >
-                Front-End Coders
-                <span class="block font-sans text-[80%] text-gray-200 xl:inline"> Mauritius 🇲🇺</span>
+                Frontend Coders
+                <span class="block font-sans text-gray-200 xl:inline capitalize font-mono"> Mauritius</span>
               </h1>
               <!-- <flip-book class="mt-[-16%] md:mt-[-12%]" /> -->
-              <p class="text-md md:text-lg text-gray-300 font-mono">
+              <p class="text-md md:text-lg text-gray-300 font-mono font-bold">
                 Front-End coders Mauritius is a free meetup that's dedicated to
                 all aspect of Front-End development. it occurs in general one a
                 month at announced date and place. Feel free to join!
@@ -27,7 +27,7 @@
               <div>
                 <router-link
                   :to="{ path: '/meetups' }"
-                  class="text-md w-48 rounded-md bg-blue-600 px-4 py-4 text-center font-medium text-white md:w-64 md:px-8 md:text-xl"
+                  class="text-md block w-48 rounded-md bg-blue-600 px-4 py-4 text-center font-medium text-white md:w-64 md:px-8 md:text-xl"
                 >
                   View all meetups
                 </router-link>
@@ -74,9 +74,15 @@
   border-radius: 50%;
   width: var(--size);
   height: var(--size);
-  background-image: linear-gradient(-45deg, #fff 50%, #006eff 50%);
+  background-image: linear-gradient(-45deg, #006eff50 50%, #006eff 50%);
   filter: blur(100px);
   transform: translate(-50%, -50%);
+  transition: all 1s ease-in-out;
+}
+
+.image-container:hover  .image-bg {
+    background-position: -300px -300px;
+    transition: all 0.3s ease-in-out;
 }
 
 @media  (min-width: 768px) {
