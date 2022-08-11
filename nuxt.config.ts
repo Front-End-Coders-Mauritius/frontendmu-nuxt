340e78001d747610aa42bd96e4509a6362d476c0
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-08-11 18:02:53 +0400

    fixed openblock error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,9 @@ export default defineNuxtConfig({
       },
     },
   },
+  vite: {
+    resolve: { dedupe: ['vue'] },
+  },
   css: [
     '~/assets/css/styles.css',
     '~/assets/css/tailwind.css',
