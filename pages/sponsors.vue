eca798fa7f6647e38eb0d44cc0782acdd10c3cbd
Kushul Soomaree <kushul@lsl.digital>
2022-08-31 17:46:05 +0400

    Sponsor Page Tiers + listing ui

--- pages/sponsors.vue
+++ pages/sponsors.vue
@@ -6,63 +6,70 @@ definePageMeta({
   },
 })
 
-const sponsors = [
+const sponsorsTypes = [
   {
-    name: 'Sandeep Ramgolam',
-    role: 'Lead Organiser',
-    imageUrl:
-      'https://github.com/MrSunshyne.png',
+    title: 'Domain Sponsor',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
+    sponsors: [
+        {
+            name: "Ringier SA",
+            sponsorUrl: 'https://www.ringier.com/about-us/south-africa/',
+        }
+    ]
   },
-  {
-    name: 'Kushul Soomaree',
-    role: 'Organiser',
-    imageUrl:
-      'https://github.com/kushul.png',
+   {
+    title: 'Goodies Sponsors',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    sponsors: [
+        {
+            name: "Ringier SA",
+            sponsorUrl: 'https://www.ringier.com/about-us/south-africa/',
+        }
+    ]
   },
-  {
-    name: 'Michaël Jules',
-    role: 'Active Community Member',
-    imageUrl:
-      'https://github.com/mgjules.png',
-  },
-  {
-    name: 'Mahima Ramgolam',
-    role: 'Active Community Member',
-    imageUrl:
-      'https://github.com/mahima98.png',
-  },
-  {
-    name: 'Saamiyah Peerun',
-    role: 'Active Community Member',
-    imageUrl:
-      'https://github.com/saamiyah.png',
+   {
+    title: 'Meetup Sponsors',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    sponsors: [
+        {
+            name: "Ringier SA",
+            sponsorUrl: 'https://www.ringier.com/about-us/south-africa/',
+        },
+        {
+            name: "Ringier SA",
+            sponsorUrl: 'https://www.ringier.com/about-us/south-africa/',
+        },
+        {
+            name: "Ringier SA",
+            sponsorUrl: 'https://www.ringier.com/about-us/south-africa/',
+        }
+    ]
   },
 ]
 </script>
 
 <template>
   <div class="container max-w-7xl mx-auto">
-
     <div class="bg-white">
-      <div class="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
+      <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
         <div class="space-y-8 sm:space-y-12">
-          <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
-            <h2 class="text-3xl font-bold tracking-tight sm:text-5xl sm:tracking-tight">Our Sponsors</h2>
+          <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl text-center">
+            <h2 class="text-3xl font-bold tracking-tight sm:text-5xl sm:tracking-tight">Sponsors</h2>
             <p class="text-xl text-gray-500">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
           </div>
-          <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
-            <h2 class="text-3xl font-bold tracking-tight sm:text-4xl sm:tracking-tight">Domain Sponsor</h2>
-            <p class="text-xl text-gray-500">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-          </div>
-          <ul role="list" class="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6">
-            <li>
-                <div class="flex flex-col justify-center items-center">
-                    <a target="_blank" href="https://www.ringier.com/about-us/south-africa/">
+          <div v-for="(sponsorType, index) in sponsorsTypes" :key="index">
+            <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+                <h2 class="text-3xl font-bold tracking-tight sm:text-4xl sm:tracking-tight font-mono">{{ sponsorType.title }}</h2>
+                <p class="text-xl text-gray-500">{{sponsorType.description}}</p>
+            </div>
+            <ul role="list" class="mx-auto mt-6 grid grid-cols-1 gap-x-4 gap-y-2 sm:grid-cols-3 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-3">
+                <li v-for="sponsor in sponsorType.sponsors" :key="sponsor.name">
+                    <a target="_blank" :href="sponsor.sponsorUrl" class="mt-4 md:mt-0 relative rounded-xl flex justify-center items-center h-40 flex-col gap-2 group bg-white p-6 shadow-md transition-all hover:shadow-lg border-t-8 border-solid border-blue-500">
                         <logo-ringier class="w-48 h-auto opacity-80" />
                     </a>
-                </div>
-            </li>
-          </ul>
+                </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
